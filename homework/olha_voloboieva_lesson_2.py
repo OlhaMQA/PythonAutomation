ee99343b2895ad18dab3dee32e82bda30eed52a6
Olha <olga.voloboyeva@gmail.com>
2023-02-19 01:46:59 +0100

    Lesson 2 Homework

---
+++ homework/olha_voloboieva_lesson_2.py
@@ -0,0 +1,37 @@
+from math import pi
+
+# Task 1
+first_name = input("please, enter your first name:\n")
+last_name = input("please, enter your last name:\n")
+full_name = first_name + " " + last_name
+
+print('\nYou are: ' + full_name)
+print('\nIn Upper register: ' + full_name.upper())
+print('\nIn Lower register: ' + full_name.lower())
+print('\nEach word capitalised: ' + full_name.title())
+print('\nFive times in a row: '+(full_name + " ")*5)
+
+first_name = "   \t\n   " + first_name + "   \t\n   "
+
+print("\nUpdated name with whitespaces: " + first_name)
+
+first_name = first_name.strip()
+
+print("\nUpdated name stripped: " + first_name)
+
+# Task 2
+
+radius = float(input('Enter radius: '))
+
+square = pi*radius**2
+circuit = pi*radius*2
+
+print(f'Square is: {round(square, 2)}\nCircuit is: {round(circuit, 2)}')
+
+# Task 3
+
+dollar_exchange_rate = 36.42
+uah_amount = float(input('Enter amount: '))
+usd_amount = round(uah_amount/dollar_exchange_rate, 2)
+
+print(f'Поточний курс складає: {uah_amount} грн. = {usd_amount} доларів')
