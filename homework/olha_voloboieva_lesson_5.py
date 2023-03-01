ee5adcddbce4949b721ad99cf747cd57b541bc57
Olha <olga.voloboyeva@gmail.com>
2023-03-01 20:27:47 +0100

    Homework 5

--- homework/olha_voloboieva_lesson_5.py
+++ homework/olha_voloboieva_lesson_5.py
@@ -1,13 +1,15 @@
 import random
 
 # Task 1
-numbers_1 = [1, 2, 3, 4, 5, 5, 8, 9, 6.11, 6]               # 1st List of numbers given
-numbers_2 = [1, 6.11, 6, 8, 6, 0, -6, 4.6, 3, 4, 7]         # 2nd List of numbers given
+numbers_1 = random.choices(range(11), k=10)               # 1st List of numbers generated
+numbers_2 = random.choices(range(11), k=10)               # 2nd List of numbers generated
+print(f'List 1: {numbers_1}\nList 2: {numbers_2}')        # Given lists printed for visibility
 
-common_numbers = list(set(numbers_1).intersection(set(numbers_2)))      # List of common numbers of two given lists
+common_numbers = list(set(numbers_1).intersection(set(numbers_2)))      # List of common numbers of two lists
 
+print('Common numbers: ')
 for number in sorted(common_numbers):
-    print(number)                                                       # Output: numbers common for both lists
+    print(number)                                      # Output: common numbers of both lists, sorted by asc
 
 # Task 2
 students_score = {                                                      # Dictionary with students' names and scores
@@ -19,6 +21,7 @@ students_score = {                                                      # Dictio
 
 average = sum(students_score.values())/len(students_score)      # Calculation of an average score of the class
 
+print(f'Average score is {average}\nStudents exceeding the average score:')   # Description printed for visibility
 for student in students_score:
     if students_score[student] > average:
         print(student)                            # Output: Names of the students, whose score is greater than average
@@ -27,17 +30,17 @@ for student in students_score:
 numbers_list = random.choices(range(11), k=10)    # List of 10 elements, containing randomly generated numbers 0-10
 
 print(numbers_list)                               # Output: Numbers List printed for visibility
-print(f'Кількість різних значень: {len(set(numbers_list))}')       # Output: Quantity of unique values
+print(f'Кількість унікальних значень: {len(set(numbers_list))}')       # Output: Quantity of unique values
 
 # Task 4
-# User enters 2 lists of numbers separated by whitespaces, which are split and transformed to sets
-user_numbers_1 = set(input('Enter a first list of numbers, separated by whitespace: ').split())
-user_numbers_2 = set(input('Enter a second list of numbers, separated by whitespace: ').split())
+# 2 lists of 10 elements, containing randomly generated numbers 0-10
+user_numbers_1 = random.choices(range(11), k=10)
+user_numbers_2 = random.choices(range(11), k=10)
 
-# List of common numbers of 2 sets, sorted by ascending
-common_list = sorted(list(user_numbers_1.intersection(user_numbers_2)))
+# List of common numbers of 2 sets, sorted by asc
+common_list = sorted(list(set(user_numbers_1).intersection(set(user_numbers_2))))
 
-print('Common numbers of your lists are: ', end='')   # Description of output printed
+print(f'List 1: {user_numbers_1}\nList 2: {user_numbers_2}\nCommon numbers:')      # Given lists printed for visibility
 for element in common_list:
     print(int(element), end=' ')                      # Common numbers printed in the same line
 print()                                               # Empty string printed to separate the next task
