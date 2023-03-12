6ba72e63585ef13fe55a7805d80858c52f179bd5
Olha <olga.voloboyeva@gmail.com>
2023-03-12 17:43:47 +0100

    Homework 7. Part 2

--- homework/olha_voloboieva_lesson_7_p2.py
+++ homework/olha_voloboieva_lesson_7_p2.py
@@ -6,12 +6,12 @@ def create_email(domains_list, names_list):
     """Generates email in specified format. Names and Domains are lists of strings.
 Returns an email string"""
 
-    name = choice(names_list)
-    number = randint(100, 999)
-    random_string = ''.join(sample(ascii_lowercase, randint(5, 7)))
-    domain = choice(domains_list)
+    name = choice(names_list).lower()       # Selects random name from the list in lowercase
+    number = randint(100, 999)              # Selects a random number
+    random_string = ''.join(sample(ascii_lowercase, randint(5, 7)))     # Selects 5-7 random letters from a to z
+    domain = choice(domains_list)           # Selects random domain name from the list
 
-    return f'{name}.{number}@{random_string}.{domain}'
+    return f'{name}.{number}@{random_string}.{domain}'                  # Returns an email composed of the choices
 
 
 names = ['Test', 'name', 'John', 'Mary', 'Anna', 'hillel']
