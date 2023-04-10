0c696c6d73b56f31323b15b90a652eec2f96aaf8
Olha <olga.voloboyeva@gmail.com>
2023-04-10 20:29:54 +0200

    Homework 10. Decorators, list comprehensions

--- homework/Practice.py
+++ homework/Practice.py
@@ -1,19 +1,38 @@
-# num = input('Enter number: ')
-#
-# user_nums = []
-#
-# while num != 'sum':
-#     try:
-#         user_nums.append(float(num))
-#         num = input('Enter number: ')
-#     except ValueError:
-#         print('Enter valid number or "sum"')
-#         num = input('Enter number: ')
-#         continue
-#
-# print(sum(user_nums))
-#
-#
-# a=[1,2,3,4,5,6,7,8,9]
-#
-# print(a[::2])
+class Cats:
+
+    def __init__(self, name, age, breed, colour, character):
+        self.gender = None
+        self.name = name
+        self.age = age
+        self.breed = breed
+        self.colour = colour
+        self.character = character
+
+
+    def is_adult(self):
+        if self.age > 2:
+            return True
+        else:
+            return False
+
+
+    def gender(self):
+        list_of_boys_names = ['Barsik', 'Cat2']
+        if self.name in list_of_boys_names:
+            self.gender = 'boy'
+        else:
+            self.gender = 'girl'
+
+
+cat = Cats('Barsik', 1, 'no name', 'black', 'good boy')
+
+print(cat.age)
+print(cat.name)
+
+cat_girl = Cats('Murka', 15, 'persian', 'red', 'bad')
+
+print(cat.is_adult())
+print(cat_girl.is_adult())
+
+cat.gender()
+print(cat.gender)
