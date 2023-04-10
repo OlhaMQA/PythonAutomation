680cdb16961aba41d1bc1ee88ae23bb01bb84cc2
Olha <olga.voloboyeva@gmail.com>
2023-04-10 18:28:17 +0200

    Homework 8. lib

--- homework/olha_voloboieva_lesson_10.py
+++ homework/olha_voloboieva_lesson_10.py
@@ -1,26 +1,28 @@
 import datetime
 
 
-# Exception
+# Exception handling
 def division_func(a, b):
-    try:
+    try:                                        # Try to divide the numbers, return the result
         return a/b
-    except ZeroDivisionError:
+    except ZeroDivisionError:                   # if ZeroDivisionError is raised, return 'n/a' and print error text
         print('Division by 0 is Not allowed')
         return 'n/a'
 
 
-# Console output of exception
+# Console output of exception handling
 division_func(20, 0)
 
 
-# Date
+# Date calculation
 def calculate_date(date, days, boolean):
-    delta = datetime.timedelta(days=days)
+    delta = datetime.timedelta(days=days)                # Create delta from the days
+    # Calculate new date: add delta if boolean = True, otherwise - subtract
     new_date = date + (delta if boolean else delta*(-1))
-    return new_date
+    return new_date                                     # Return new date
 
 
+# Date calculation demonstration
 date_today = datetime.datetime.now()
 
 yesterday = calculate_date(date_today, 1, False)
@@ -30,21 +32,14 @@ print(f'Yesterday was {yesterday}')
 print(f'Tomorrow will be {tomorrow}')
 
 
-# Birthday
+# Calculate exact age by birthdate
 def calculate_age(birthdate):
     age_in_days = (datetime.datetime.today() - birthdate).days
     birth_timestamp = datetime.datetime.timestamp(birthdate)
     return age_in_days, birth_timestamp
 
-
+# Demonstration of age calculation (return age and timestamp)
 my_age, my_birth_timestamp = calculate_age(datetime.datetime(1991, 9, 18, 9, 16, 00, 000))
 
 print(f'I\'m {my_age} days old')
 print(f'Timestamp of my birthday is {my_birth_timestamp}')
-
-
-
-
-
-
-
--- homework/olha_voloboieva_lesson_11.py
+++
@@ -1,3 +0,0 @@
-from homework import out_lib
-
-out_lib.library_math.arithmetics.add()
